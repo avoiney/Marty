732956256e8c630dc59b0be2a12e09d7b3bdb6c3
Alex V <bonjour@alexandre.voiney.fr>
2017-08-03 11:25:29 +0200

    Implement a basic diff command

--- marty/commands/mount.py
+++ marty/commands/mount.py
@@ -3,6 +3,7 @@ import os
 import os.path
 import subprocess
 
+from marty.printer import printer
 from marty.commands import Command
 from marty.operations.fs import MartyFS
 
@@ -51,3 +52,35 @@ class Explore(Command):
             pshell = subprocess.Popen([shell], cwd=fullname)
             pshell.wait()
             fs.umount()
+
+
+class Diff(Command):
+
+    """ Make a diff of two backups or tree for a remote.
+    """
+
+    help = 'Make a diff of two backups or tree for a remote'
+
+    def prepare(self):
+        self._aparser.add_argument('remote', nargs='?')
+        self._aparser.add_argument('names', nargs=2)
+
+    def run(self, args, config, storage, remotes):
+        names = ['%s/%s' % (args.remote, name) if args.remote else name for name in args.names]
+        trees = [storage.get_tree(name) for name in names]
+
+        with tempfile.TemporaryDirectory() as fullname1, tempfile.TemporaryDirectory() as fullname2:
+            fs1 = MartyFS(storage, trees[0], fullname1)
+            fs2 = MartyFS(storage, trees[1], fullname2)
+            fs1.mount()
+            fs2.mount()
+            pshell = subprocess.Popen(['diff', '-q', '-r', '--no-dereference', '--suppress-common-lines',
+                                       fullname1, fullname2], stdout=subprocess.PIPE)
+            pshell.wait()
+            output = pshell.stdout.read().decode('utf8')
+            fs2.umount()
+            fs1.umount()
+            names_filenames = zip((fullname1, fullname2), names)
+            for name_filename in names_filenames:
+                output = output.replace(*name_filename)
+            printer.p('Diff output betwwen <b>%s</b> and <b>%s</b>:\n\n%s' % (*names, output))
